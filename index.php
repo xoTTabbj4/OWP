16e9c20809608b4c13b5e5a5cf5db697157b04d1
xoTTabbj4 <xottabbj4@ukr.net>
2020-06-15 12:04:50 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="utf-8">
+    <title>Приклад сторінки</title>
+</head>
+<body>
+<header>
+    <?php include ("menu.php");?>
+</header>
+<div class="divtxt">
+    <p>Ознайомча сторінка Пуделка Олександра</p>
+</div>
+
+</body>
+</html>
