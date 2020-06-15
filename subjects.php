41b5eec08d6125407dafec9a179cf7de00c913bf
xoTTabbj4 <xottabbj4@ukr.net>
2020-06-15 12:04:58 +0300

    Create subjects.php

---
+++ subjects.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="utf-8">
+    <title>Приклад сторінки</title>
+</head>
+<body>
+<header>
+    <?php include ("menu.php");?>
+</header>
+<div class="tablediv">
+    <fieldset>
+        <legend>Навчальний план</legend>
+            <table>
+                <tr>
+                    <td><b>Предмети:</b></td>
+                    <td>ОВП</td>
+                    <td>Правознавство</td>
+                    <td>ТАК</td>
+                    <td>СПІМ</td>
+                    <td>ЕТМТ</td>
+                    <td>ЧММ на ЕОМ</td>
+                    <td>АТП</td>
+                </tr>
+                <tr>
+                    <td><b>години:</b></td>
+                    <td>10</td>
+                    <td>30</td>
+                    <td>50</td>
+                    <td>60</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>70</td>
+                </tr>
+                <tr>
+                    <td><b>години лекції:</b></td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>40</td>
+                    <td>60</td>
+                    <td>80</td>
+                    <td>90</td>
+                    <td>40</td>
+                </tr>
+                <tr>
+                    <td><b>години практичні:</b></td>
+                    <td>70</td>
+                    <td>60</td>
+                    <td>50</td>
+                    <td>100</td>
+                    <td>40</td>
+                    <td>10</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td><b>години лабораторні:</b></td>
+                    <td>30</td>
+                    <td>20</td>
+                    <td>10</td>
+                    <td>100</td>
+                    <td>90</td>
+                    <td>70</td>
+                    <td>90</td>
+                </tr>
+                <tr>
+                    <td><b>Кредити:</b></td>
+                    <td>30</td>
+                    <td>20</td>
+                    <td>100</td>
+                    <td>100</td>
+                    <td>90</td>
+                    <td>70</td>
+                    <td>90</td>
+                </tr>
+            </table>
+    </fieldset>
+</div>
+</body>
+</html>
